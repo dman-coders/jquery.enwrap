10d82415a1427b7f59b7e2bf0562725ecd1bfc32
Dan Morrison <dan@coders.co.nz>
2013-11-20 01:58:53 +1300

    renamed vars from camelCase to drupal_code style for compatability

--- jquery.enwrap.js
+++ jquery.enwrap.js
@@ -55,8 +55,8 @@
  * To make it run on h3s and h2s, run twice with two parameters.
  * Low first.
  *   $('.content')
- *     .enwrap({ startLevel: 3 })
- *     .enwrap({ startLevel: 2 }); 
+ *     .enwrap({ start_level: 3 })
+ *     .enwrap({ start_level: 2 }); 
  * 
  */
 (function ($) {
@@ -69,49 +69,49 @@
     var settings = $.extend({
       // These are the defaults.
       // Which H tags should be the root items. h1 = 1, h2 = 2, etc.
-      startLevel: 2,
-      // How many levels of H tags should be processed, including the startLevel
+      start_level: 2,
+      // How many levels of H tags should be processed, including the start_level
       depth: 3,
       // CSS class to apply to each section wrapper div
-      sectionClass: "section",
+      section_class: "section",
       // CSS class to apply to each section content wrapper div
-      contentClass: "section-content",
+      content_class: "section-content",
     }, options );
     
     var scope = $(this);
-    var headerTag = 'H' + settings.startLevel;
+    var headerTag = 'H' + settings.start_level;
     
     // Need to make sure I don't chunk above my requested level.
     var higherTags = [];
-    for (var t=settings.startLevel-1;t>0;t--) {
+    for (var t=settings.start_level-1;t>0;t--) {
       higherTags['H' + t] = 'H' + t;
     }
 
     // Check each element that is a direct child of the scope.
     // Pick them off one by one and sort them into more organized boxes.
-    var currentSection;
-    var currentSectionContent;
-    currentSectionContent = scope;
+    var current_section;
+    var current_section_content;
+    current_section_content = scope;
     
     scope.children().each(function (index, item) {
       if (higherTags[$(this)[0].nodeName]){
         // If I find higher tags than expected, stop what I'm doing and 
         // close the current container. That prevents H2s from swallowing H1s 
         // etc. This makes it safe to run through twice to create nesting.
-        currentSectionContent = scope;
-        currentSectionContent.append(item);
+        current_section_content = scope;
+        current_section_content.append(item);
      }
       else if ($(this)[0].nodeName == headerTag){
         // Found a heading. Open a new div and shift all subsequent elements
         // into that until the next heading comes along.
-        currentSection = $('<div class="' + settings.sectionClass + '" />');
-        scope.append(currentSection);
-        currentSection.append(item);
-        currentSectionContent = $('<div class="' + settings.contentClass + '" />');
-        currentSection.append(currentSectionContent)
+        current_section = $('<div class="' + settings.section_class + '" />');
+        scope.append(current_section);
+        current_section.append(item);
+        current_section_content = $('<div class="' + settings.content_class + '" />');
+        current_section.append(current_section_content)
       }
       else {
-        currentSectionContent.append(item);
+        current_section_content.append(item);
       }
     });
     
